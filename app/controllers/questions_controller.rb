83c9cfbb2383fcf0ba97406b877f7fbce0c8d7ad
Valeria Behrend <valu-sp@hotmail.com>
2021-07-06 19:31:52 +0200

    logic done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,9 +7,13 @@ class QuestionsController < ApplicationController
 
     def answer
         @answer = params[:question]
-        message = ""
+        @message = ""
         if @answer === 'I am going to work'
-            message = 'Great!'
+            @message = 'Great!'
+        elsif @answer.include?('?')
+            @message = 'Silly question, get dressed and go to work!'
+        else
+            @message = 'I dont care, get dressed and go to work!'
         end
     end
 end
